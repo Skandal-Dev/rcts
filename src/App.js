efe67c565959038d226d0add664972d99effacc9
⺞ Skandal ⺞ <95112743+Skandal-Dev@users.noreply.github.com>
2021-11-27 08:32:03 +0000

    Add user interface

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.png';
+import './App.css';
+import Layout from './module/Layout.js';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Layout />
+    </div>
+  );
+}
+
+export default App;
